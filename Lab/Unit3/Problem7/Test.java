ae6e790988023508922d8ce7d70a5c5322daba31
khebinhh <kramirez101@student.rccd.edu>
2022-07-12 20:49:27 -0700

    update on +=

--- Lab/Unit3/Problem7/Test.java
+++ Lab/Unit3/Problem7/Test.java
@@ -4,12 +4,15 @@ class Test {
 
         double avg[] = new double[6];    // average of test scores
 
+        double average;
+
         for (int i = 0; i < 6; i++) {
             avg[i] = nums[i] / 6;
+            average += avg[i];
         }
-        double average = avg[0] + avg[1] + avg[2] + avg[3]
+        /*double average = avg[0] + avg[1] + avg[2] + avg[3]
                         + avg[4] + avg[5];
-
+        */
         System.out.println("This program will display the average of these test scores:");
         System.out.println("[89, 91, 76, 83, 90, 79]");
         System.out.println("Answer: " + average);
