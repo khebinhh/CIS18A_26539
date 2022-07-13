f74dcf4f60bc24a4747b1cbf1c5ebe392271f54e
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-07-12 17:33:42 -0700

    update

---
+++ Lab/Unit3/Problem9/Pizza.java
@@ -0,0 +1,21 @@
+/*  9. A pizzeria offers discount coupons to its customers 
+        when they purchase multiple pizzas at the restaurant:
+        •	$2 off for 2 pizzas.
+        •	$5 off for 4 pizzas
+        •	$10 off for 8 pizzas
+        •	$12 off for 10 pizzas
+        •	$15 off for 12 pizzas
+    Create, compile and run the program that calculates and 
+    displays the average discount per pizza at the restaurant. 
+    The program should contain two dimensional array, 
+    appropriate variables and data type.
+ */
+class Pizza {
+    double pizzas[][] = {{2, 2}, {5, 4}, {10, 8}, {12, 10}, {15, 12}};
+    double discount = 0;
+
+    for(int i = 0; i < 5; i++) {
+        System.out.println("When you order " + pizzas[i][1] + "pizzas,");
+        System.out.println("You get a discount of $" + pizza[i][0] + " dollars.");
+    }
+}
