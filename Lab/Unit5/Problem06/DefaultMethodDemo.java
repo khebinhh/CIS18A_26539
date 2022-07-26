32d7bf5552b472b926d460e7461b348bb256d248
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-07-25 23:02:15 -0700

    so close

---
+++ Lab/Unit5/Problem06/DefaultMethodDemo.java
@@ -0,0 +1,23 @@
+public interface MyIF{
+    int getUserId();
+
+    default int getAdminID(){
+        return 1;
+    }
+}
+
+class MyIFImp implements MyIF{
+    public int getUserId() {
+        return 100;
+    }
+}
+
+class DefaultMethodDemo{
+    public static void main(String args[]){
+        MyIFImp obj = new MyIFImp();
+
+        System.out.println("User ID is " + obj.getUserId());
+
+        System.out.println("Admin ID is " + obj.getAdminID());
+    }
+}
